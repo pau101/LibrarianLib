1e0ba98c4e8ecc55396dd9d225a8d561eb354688
Pierce Corcoran <pierce@plasticcow.com>
2016-09-08 00:14:53 -0700

    More draggy doohickies

--- src/main/java/com/teamwizardry/librarianlib/client/newbook/editor/GuiBookLayoutEditor.kt
+++ src/main/java/com/teamwizardry/librarianlib/client/newbook/editor/GuiBookLayoutEditor.kt
@@ -4,6 +4,7 @@ import com.teamwizardry.librarianlib.client.gui.GuiBase
 import com.teamwizardry.librarianlib.client.gui.GuiComponent
 import com.teamwizardry.librarianlib.client.gui.components.ComponentCenterAlign
 import com.teamwizardry.librarianlib.client.gui.components.ComponentRect
+import com.teamwizardry.librarianlib.client.gui.components.ComponentText
 import com.teamwizardry.librarianlib.client.gui.components.ComponentVoid
 import com.teamwizardry.librarianlib.client.gui.mixin.DragMixin
 import com.teamwizardry.librarianlib.client.gui.mixin.DrawBorderMixin
@@ -13,11 +14,14 @@ import com.teamwizardry.librarianlib.client.sprite.Texture
 import com.teamwizardry.librarianlib.common.util.math.Vec2d
 import net.minecraft.util.ResourceLocation
 import java.awt.Color
+import java.util.concurrent.ThreadLocalRandom
 
 /**
  * Created by TheCodeWarrior
  */
 class GuiBookLayoutEditor(val book: Book) : GuiBase(0, 0) {
+
+    var selected: GuiComponent<*>? = null
 
     companion object {
         val TEX = Texture(ResourceLocation("librarianlib:textures/gui/editorUI.png"))
@@ -37,6 +41,7 @@ class GuiBookLayoutEditor(val book: Book) : GuiBase(0, 0) {
     val mainComponent = ComponentVoid(0,0, bookWidth, bookHeight)
     val sidebarList = ComponentRect(0, 0, 100, 0)
     val sidebarInfo = ComponentRect(0, 0, 100, 0)
+    val sprites = ComponentVoid(0, 0, 0, 0)
 
     init {
 
@@ -54,12 +59,63 @@ class GuiBookLayoutEditor(val book: Book) : GuiBase(0, 0) {
         fullscreenComponents.add(sidebarList)
         fullscreenComponents.add(sidebarInfo)
 
-        var rect = ComponentRect(0, 0, 25, 25)
-        rect.color.func { Color.CYAN }
+        val pos = ComponentText(1, 1, ComponentText.TextAlignH.LEFT, ComponentText.TextAlignV.TOP)
+        val size = ComponentText(1, 10, ComponentText.TextAlignH.LEFT, ComponentText.TextAlignV.TOP)
+
+        pos.text.func {
+            val sel = selected
+            if(sel == null)
+                "(N/A, N/A)"
+            else
+                "(${sel.pos.xi}, ${sel.pos.yi})"
+        }
+
+        size.text.func {
+            val sel = selected
+            if(sel == null)
+                "N/A x N/A"
+            else
+                "${sel.size.xi} x ${sel.size.yi}"
+        }
+
+        sidebarInfo.add(pos)
+        sidebarInfo.add(size)
+
+        sprites.calculateOwnHover = false
+        components.add(sprites)
+
+        val componentsbg = ComponentVoid(-1000, -1000, 1000, 1000)
+        componentsbg.zIndex = -1000
+        componentsbg.BUS.hook(GuiComponent.MouseDownEvent::class.java) {
+            if(componentsbg.mouseOver) selected = null
+        }
+        sprites.add(componentsbg)
+
+        val rect = createRect()
+        createRect()
+        createRect()
+        createRect()
+        createRect()
+        createRect()
+        createRect()
+        createRect()
+
+        selected = rect
+
+
+    }
+
+    fun createRect(): GuiComponent<*> {
+        val rect = ComponentRect(0, 0, 25, 25)
+        val color = Color(ThreadLocalRandom.current().nextInt(0, 255), ThreadLocalRandom.current().nextInt(0, 255), ThreadLocalRandom.current().nextInt(0, 255))
+        rect.color.func { color }
         ResizableMixin(rect, 8, false)
         DragMixin(rect, { it })
-        components.add(rect);
 
+        rect.BUS.hook(GuiComponent.MouseDownEvent::class.java) { if(rect.mouseOver) selected = rect }
+
+        sprites.add(rect)
+        return rect
     }
 
     fun resize(size: Vec2d) {
